3e0ce898528f67a3bab0a47c662f8d1d78aa5981
JonathanDog <rangersps42@gmail.com>
2022-01-04 16:08:48 -0500

    Added working routing

--- reasturant_app/src/views/Home.vue
+++ reasturant_app/src/views/Home.vue
@@ -1,15 +1,34 @@
 <template>
-  <hello-world />
+  <v-app id="inspire">
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+
+
+          <v-col
+            cols="12"
+            sm="12"
+          >
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+              Home
+            </v-sheet>
+          </v-col>
+
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+  export default {
+    data: () => ({
 
-export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
-  },
-};
+    }),
+  }
 </script>
