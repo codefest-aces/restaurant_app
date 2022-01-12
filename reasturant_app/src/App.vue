675d34862c1c821c75b0f9a9838ed9b5dda323d0
JonathanDog <rangersps42@gmail.com>
2022-01-12 16:22:59 -0500

    Added vueX to store the api call globally

--- reasturant_app/src/App.vue
+++ reasturant_app/src/App.vue
@@ -32,7 +32,18 @@
 
     </v-app-bar>
 
-  <router-view></router-view>
+
+  <div v-if="gotData">
+    <router-view ></router-view>
+  </div>
+  <div v-else>
+    <v-main>
+        <h1>Loading...</h1>
+    </v-main>
+
+  </div>
+
+
   </v-app>
 </template>
 
@@ -49,7 +60,31 @@
         '/',
         'filter',
         'about'
-      ]
+      ],
+      gotData: false
     }),
+    async mounted () {
+      const axios = require('axios').default;
+
+      let response = await axios.get("https://data.cityofnewyork.us/resource/43nn-pn8j.json?$where=inspection_date > '2020-01-01T00:00:00.000'&$limit=100000");
+
+      let data = response.data
+      let distinct = []
+      let results = []
+      for(let i = 0; i < data.length; i++) {
+        if(!distinct.includes(data[i].camis)) {
+
+          distinct.push(data[i].camis)
+          results.push(data[i])
+
+        }
+      }
+      //console.log(results)
+
+
+      this.$store.state.apiData = results
+      this.gotData = true
+      console.log(this.$store.state.apiData)
+    }
   }
 </script>
